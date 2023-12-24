ac8869742fe804e09317066a30a9b27d449bed36
Alexander Stekelenburg <anoniem.axander@gmail.com>
2023-12-24 23:12:26 +0100

    Day 24 cleanup

--- src/day24.rs
+++ src/day24.rs
@@ -4,7 +4,7 @@ use crate::common;
 use itertools::Itertools;
 use z3::{
     ast::{Ast, Int},
-    Config, Context, Solver,
+    Config, Context, SatResult, Solver,
 };
 
 #[allow(clippy::similar_names)]
@@ -78,64 +78,13 @@ pub fn main() -> Result<(i64, i64)> {
         solver.assert(&(z + vz * &t)._eq(&(&start_z + &start_vz * &t)));
     }
 
-    solver.check();
-    let model = solver.get_model().expect("Not satisfiable");
+    assert_eq!(solver.check(), SatResult::Sat, "Not satisfiable");
+    let model = solver.get_model().unwrap();
     let solution_b = model
         .eval(&(start_x + start_y + start_z), true)
         .unwrap()
         .as_i64()
         .unwrap();
-
-    /*
-    let mut points = Vec::new();
-    for combination in hailstones.windows(3) {
-        let [(A,B),(C,D),(E,F)] = combination[..] else { panic!() };
-        let ec = E.sub(&C);
-        if ec.dot(&B.cross(&D)) != 0 {
-            let s = ec.mul_const(-1).dot(&A.sub(&C).cross(&D))/ec.dot(&B.cross(&D));
-            let pos = A.add(&B.mul_const(s));
-            if s > 0 {
-            points.push((s,pos));
-            }
-        }
-    }
-    points.sort();
-    for (s,pos) in points {
-        println!("{s}, {} {} {}", pos.x, pos.y, pos.z);
-    }
-    */
-    // let A = Point::new(19, 13, 30);
-    // let B = Point::new(-2,1,-2);
-    // let C = Point::new(18, 19, 22);
-    // let D = Point::new(-1,-1,-2);
-    // let E = Point::new(20, 25, 34);
-    // let F = Point::new(-2,-2,-4);
-
-    // let s = 5;
-    // let x = D.z*(A.y - C.y) + D.y*(A.z - C.z) + s*(D.z*B.y - D.y*B.z);
-    // let y = D.x*(A.z - C.z) + D.z*(A.x - C.x) + s*(D.x*B.z - D.z*B.x);
-    // let z = D.y*(A.x - C.x) + D.x*(A.y - C.y) + s*(D.y*B.x - D.x*B.y);
-
-    // let pq = A.add(&B.mul_const(s)).sub(&C);
-    // let normal = pq.cross(&D);
-    // println!("{} {} {}", normal.x, normal.y, normal.z);
-
-    // let ec = E.sub(&C);
-    // let res = ec.dot(&normal);
-    // println!("{}", res);
-
-    // let a =
-    //     ec.mul_const(-1).dot(
-    //         &D.zxy().mul(&A.yzx().sub(&C.yzx())).add(
-    //         &D.yzx().mul(&A.zxy().sub(&C.zxy()))));
-    // let b = ec.dot(&D.zxy().mul(&B.yzx()).sub(&D.yzx().mul(&B.zxy())));
-
-    // println!("{a}/{b}");
-
-    // let a = ec.mul_const(-1).dot(
-    //         &A.sub(&C).cross(&D));
-    // let b = ec.dot(&B.cross(&D));
-    // println!("{a}/{b}");
 
     Ok((solution_a, solution_b))
 }
@@ -146,72 +95,6 @@ struct Point {
     y: i64,
     z: i64,
 }
-
-/*
-impl Point {
-    fn new(x: i64, y: i64, z: i64) -> Point {
-        Point {
-            x,y,z
-        }
-    }
-
-    fn zxy(&self) -> Point {
-        Point {
-            x: self.z,
-            y: self.x,
-            z: self.y
-        }
-    }
-
-    fn yzx(&self) -> Point {
-        Point {
-            x: self.y,
-            y: self.z,
-            z: self.x
-        }
-    }
-
-    fn cross(&self, other: &Point) -> Point {
-        self.yzx().mul(&other.zxy()).sub(&self.zxy().mul(&other.yzx()))
-    }
-
-    fn dot(&self, other: &Point) -> i64 {
-        self.x * other.x + self.y * other.y + self.z * other.z
-    }
-
-    fn add(&self, other: &Point) -> Point {
-        Point {
-            x: self.x + other.x,
-            y: self.y + other.y,
-            z: self.z + other.z,
-        }
-    }
-
-    fn sub(&self, other: &Point) -> Point {
-        Point {
-            x: self.x - other.x,
-            y: self.y - other.y,
-            z: self.z - other.z,
-        }
-    }
-
-    fn mul(&self, other: &Point) -> Point {
-        Point {
-            x: self.x * other.x,
-            y: self.y * other.y,
-            z: self.z * other.z,
-        }
-    }
-
-    fn mul_const(&self, other: i64) -> Point {
-        Point {
-            x: self.x * other,
-            y: self.y * other,
-            z: self.z * other,
-        }
-    }
-}
-*/
 
 impl From<Vec3> for Point {
     fn from((x, y, z): Vec3) -> Point {
